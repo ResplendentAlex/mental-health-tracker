952b6a7e60c05d42aa2710b495cf9f1d736a6ab9
ResplendentAlex <alexanderwl295@gmail.com>
2024-09-11 15:28:37 +0700

    add: path for show_xml for the urlpatterns in urls.py

--- main/urls.py
+++ main/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from main.views import show_main, create_mood_entry
+from main.views import show_main, create_mood_entry, show_xml
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-mood-entry/', create_mood_entry, name='create_mood_entry'),
+    path('show-xml/', show_xml, name='show_xml'),
 ]
