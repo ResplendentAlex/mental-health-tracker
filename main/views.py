037b1ddd13fbcaa2e538c725cc3f65dfdc4e19fc
ResplendentAlex <alexanderwl295@gmail.com>
2024-09-11 15:35:20 +0700

    add: id sorting for both show_json and show_xml functions in views.py and its routing in urls.py

--- main/views.py
+++ main/views.py
@@ -33,3 +33,11 @@ def show_xml(request):
 def show_json(request):
     data = MoodEntry.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
