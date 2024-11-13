a51f5f5c14e352621a1ad9528bff0feb281d3591
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2024-11-13 17:30:56 +0700

    feat: finish tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
@@ -15,6 +15,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 import datetime
+import json
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -127,3 +128,21 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9rh_w5d1d8=r89_l&a2#o147d+i@1l81i1^2or6$oc#)!i8$(^
 PRODUCTION = os.getenv('PRODUCTION', False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'alexander-william-mentalhealthtracker.pbp.cs.ui.ac.id', 'https:"//alexander-william-mentalhealthtracker.pbp.cs.ui.ac.id"']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'alexander-william-mentalhealthtracker.pbp.cs.ui.ac.id', 'https:"//alexander-william-mentalhealthtracker.pbp.cs.ui.ac.id"', '10.0.2.2']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mental_health_tracker.urls'
@@ -131,3 +133,10 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
